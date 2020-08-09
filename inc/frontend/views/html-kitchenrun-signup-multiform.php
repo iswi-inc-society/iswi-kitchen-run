b3555b8f3d68baffa2656ea7139eb806414b19da
Niklas Loos <niklas.loos@live.com>
2020-08-09 16:43:51 +0200

    multiform template

--- inc/frontend/views/html-kitchenrun-signup-multiform.php
+++ inc/frontend/views/html-kitchenrun-signup-multiform.php
@@ -17,6 +17,16 @@ use League\Plates\Template\Template;
 
 <form id="kr_signup" action="<?php echo $_SERVER['REQUEST_URI'] ?>" method="post">
 
+    <ul id="kr_progressbar">
+        <li class="active"> Step 1 </li>
+        <li class="active"> Step 2 </li>
+        <li class="active"> Step 3 </li>
+    </ul>
+
+    <fieldset class="kr_tab">
+    <h2 class="kr_step_title">Step 1</h2>
+	<h3 class="kr_step_subtitle">General Information</h3>
+
     <?php $this->insert('form/html-kitchenrun-input-text', ['label' => __('Team Name', $plugin_text_domain), 'name'  => 'kr_team_name',]); ?>
 
     <?php $this->insert('form/html-kitchenrun-input-text', ['label' => __('Team Member 1', $plugin_text_domain), 'name'  => 'kr_team_member_1',]); ?>
@@ -31,6 +41,16 @@ use League\Plates\Template\Template;
 
     <?php $this->insert('form/html-kitchenrun-input-text', ['label' => __('Phone Number', $plugin_text_domain), 'name'  => 'kr_team_phone', 'type' => 'tel']); ?>
 
+    <div class="kr_form_next">
+        <input type="button" id="kr_signup_next" name="kr_next" value="<?= $this->e(__('Next', $plugin_text_domain)) ?>" style="width: 100%" onclick="nextPrev(1)">
+    </div>
+
+    </fieldset>
+
+
+    <fieldset class="kr_tab">
+    <h2 class="kr_step_title">Step 2</h2>
+	<h3 class="kr_step_subtitle">Food Preferences</h3>
 
     <div class="kr_form_checkboxes">
         <span><?= $this->e(__('Do you have any food preferences?', $plugin_text_domain)) ?></span> <br>
@@ -61,14 +81,10 @@ use League\Plates\Template\Template;
         </div>
     </div>
 
+
     <div class="kr_form_textarea">
         <label for="kr_signup_food_request"><?= $this->e(__('Other special request for food, they will be sent to the cooking team', $plugin_text_domain)) ?>:</label>
         <textarea form="kr_signup" id="kr_signup_food_request" name="kr_team_food_request">Some Text</textarea>
-    </div>
-
-    <div class="kr_form_textarea">
-        <label for="kr_signup_find_place"><?= $this->e(__('If it is not easy, give hints on how to find your place', $plugin_text_domain)) ?>:</label>
-        <textarea form="kr_signup" id="kr_signup_find_place" name ="kr_team_find_place">Some Text</textarea>
     </div>
 
     <div class="kr_form_checkboxes">
@@ -88,6 +104,27 @@ use League\Plates\Template\Template;
             <input type="checkbox" id="kr_signup_dessert" name="kr_team_dessert" checked />
             <label for="kr_signup_dessert"><?= $this->e(__('Dessert', $plugin_text_domain)) ?></label>
         </div>
+    </div>
+
+    <div class="kr_form_previous">
+        <input type="button" id="kr_signup_previous" name="kr_previos" value="<?= $this->e(__('Previous', $plugin_text_domain)) ?>" style="width: 100%" onclick="nextPrev(-1)">
+    </div>
+
+    <div class="kr_form_next">
+        <input type="button" id="kr_signup_next" name="kr_next" value="<?= $this->e(__('Next', $plugin_text_domain)) ?>" style="width: 100%" onclick="nextPrev(1)">
+    </div>
+
+    </fieldset>
+
+    
+    <fieldset class="kr_tab">
+    <h2 class="kr_step_title">Step 3</h2>
+	<h3 class="kr_step_subtitle">Other Important Infos</h3>
+
+
+    <div class="kr_form_textarea">
+        <label for="kr_signup_find_place"><?= $this->e(__('If it is not easy, give hints on how to find your place', $plugin_text_domain)) ?>:</label>
+        <textarea form="kr_signup" id="kr_signup_find_place" name ="kr_team_find_place">Some Text</textarea>
     </div>
 
     <div class="kr_form_textarea">
@@ -112,7 +149,88 @@ use League\Plates\Template\Template;
         <label for="kr_signup_privacy"><?= $this->e(__('We have read and understood the privacy agreement', $plugin_text_domain)) ?></label>
     </div>
 
-    <div class="kr_form_submit">
-        <input type="submit" id="kr_signup_submitted" name="kr_team_submitted" value="<?= $this->e(__('Sign Up', $plugin_text_domain)) ?>" style="width: 100%">
+    <div class="kr_form_previous">
+        <input type="button" id="kr_signup_previous" name="kr_previos" value="<?= $this->e(__('Previous', $plugin_text_domain)) ?>" style="width: 100%" onclick="nextPrev(-1)">
     </div>
+
+    <div class="kr_form_next">
+        <input type="button" id="kr_signup_next" name="kr_next" value="<?= $this->e(__('Next', $plugin_text_domain)) ?>" style="width: 100%" onclick="nextPrev(1)">
+    </div>
+
+    </fieldset>
+
+    <fieldset class="kr_tab">
+    <h2 class="kr_step_title">Step 4</h2>
+	<h3 class="kr_step_subtitle">Is your Input correct?</h3>
+
+    <div class="valid_data"> </div>
+
+    <div class="kr_form_previous">
+        <input type="button" id="kr_signup_previous" name="kr_previos" value="<?= $this->e(__('Previous', $plugin_text_domain)) ?>" style="width: 100%" onclick="nextPrev(-1)">
+    </div>
+
+    <div class="kr_form_submit">
+        <input type="submit" id="kr_signup_submitted" name="kr_team_submitted" value="<?= $this->e(__('Send', $plugin_text_domain)) ?>" style="width: 100%">
+    </div>
+
+    </fieldset>
+
+
 </form>
+
+
+<script>
+var currentTab = 0; // Current tab is set to be the first tab (0)
+showTab(currentTab); // Display the current tab
+
+function showTab(n) {
+  // This function will display the specified tab of the form...
+  var x = document.getElementsByClassName("kr_tab");
+  x[n].style.display = "block";
+}
+
+function nextPrev(n) {
+  // This function will figure out which tab to display
+  var x = document.getElementsByClassName("kr_tab");
+  // Exit the function if any field in the current tab is invalid:
+  //if (n == 1 && !validateForm()) return false;
+  // Hide the current tab:
+  x[currentTab].style.display = "none";
+  // Increase or decrease the current tab by 1:
+  currentTab = currentTab + n;
+  // Otherwise, display the correct tab:
+  showTab(currentTab);
+}
+
+function validateForm() {
+  // This function deals with validation of the form fields
+  var x, y, i, valid = true;
+  x = document.getElementsByClassName("kr_tab");
+  y = x[currentTab].getElementsByTagName("input");
+  // A loop that checks every input field in the current tab:
+  for (i = 0; i < y.length; i++) {
+    // If a field is empty...
+    if (y[i].value == "") {
+      // add an "invalid" class to the field:
+      y[i].className += " invalid";
+      // and set the current valid status to false
+      valid = false;
+    }
+  }
+  // If the valid status is true, mark the step as finished and valid:
+  if (valid) {
+    document.getElementsByClassName("step")[currentTab].className += " finish";
+  }
+  return valid; // return the valid status
+}
+
+function fixStepIndicator(n) {
+  // This function removes the "active" class of all steps...
+  var i, x = document.getElementsByClassName("step");
+  for (i = 0; i < x.length; i++) {
+    x[i].className = x[i].className.replace(" active", "");
+  }
+  //... and adds the "active" class on the current step:
+  x[n].className += " active";
+}
+</script>
